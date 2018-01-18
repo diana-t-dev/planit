ba6b07fc7cfa6b9e55a29dbb7679e91961bdde0a
Diana <dianaltecson@gmail.com>
2018-01-18 14:19:04 -0800

    added comments

--- client/src/components/notifications.js
+++ client/src/components/notifications.js
@@ -9,15 +9,20 @@ class Notifications extends Component {
     }
 
     componentDidMount () {
+        // set new cookie
         const cookies = new Cookies();
         cookies.set('name', 'bob');
         
+        // get request for user notifications
         axios.get(`/notifications/${cookies.get('name')}`)
         .then((results) => {
+            // set state equal to notifications
             let notifications = results.data[0].notifications;
+            // if user has none, display a message
             if (!notifications) {
                 this.setState({notifications: ["No notifications!"]})
             }
+            // else, set the state to their notifications as a list
             else {
                 notifications = notifications.split(",");
                 this.setState({notifications: notifications});  
