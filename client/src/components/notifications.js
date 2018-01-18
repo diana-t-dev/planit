f26a7a92f7628fa02d65bb3a77845e385cf0c14d
Diana <dianaltecson@gmail.com>
2018-01-18 14:12:59 -0800

    added notification conditional rendering; implemented cookies

--- client/src/components/notifications.js
+++ client/src/components/notifications.js
@@ -1,22 +1,32 @@
 import React, { Component } from "react";
 import '../App.css';
 import axios from 'axios';
+import Cookies from 'universal-cookie';
 
 class Notifications extends Component {
     state = {
-        user: 'bob',
-        notifications: ['test 1']
+        notifications: ['notification']
     }
 
     componentDidMount () {
-        axios.get(`/notifications/${this.state.user}`)
+        const cookies = new Cookies();
+        cookies.set('name', 'bob');
+        
+        axios.get(`/notifications/${cookies.get('name')}`)
         .then((results) => {
-            let notifications = results.data[0].notifications.split(",");
-            this.setState({notifications: notifications});
+            let notifications = results.data[0].notifications;
+            if (!notifications) {
+                this.setState({notifications: ["No notifications!"]})
+            }
+            else {
+                notifications = notifications.split(",");
+                this.setState({notifications: notifications});  
+            }
         })
     }
 
     render() {
+
         return (
         <div className="row">
             <div className="col s12 top z-depth-2">
@@ -30,7 +40,12 @@ class Notifications extends Component {
                 </thead>
                 <tbody>
                 {this.state.notifications.map(notification => {
-                    return <tr><td>{notification}</td><td><a className="btn">Accept</a><a className="btn">Decline</a></td></tr>
+                    if (notification === "No notifications!") {
+                        return <tr><td>{notification}</td></tr>
+                    }
+                    else {
+                        return <tr><td>{notification}</td><td><a className="btn">Accept</a><a className="btn">Decline</a></td></tr>
+                    }
                 })}
                 </tbody>
             </table>
