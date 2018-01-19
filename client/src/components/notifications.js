292bab8b8d2cd727bd4ab0889a525e34ea4ba34e
Ben Grant <Benjah88@gmail.com>
2018-01-18 22:09:24 -0800

    notifications sort of working

--- client/src/components/notifications.js
+++ client/src/components/notifications.js
@@ -3,29 +3,54 @@ import '../App.css';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
 
+var noteys = [];
+
+
 class Notifications extends Component {
     state = {
-        notifications: ['notification']
+        notifications: []
+     
     }
 
     componentDidMount () {
+
+    	 noteys = [];
         // set new cookie
         const cookies = new Cookies();
-        cookies.set('name', 'bob');
+        cookies.set('name', 'Jesus');
         
         // get request for user notifications
         axios.get(`/notifications/${cookies.get('name')}`)
         .then((results) => {
+
+        	console.log(results)
             // set state equal to notifications
-            let notifications = results.data[0].notifications;
+           
             // if user has none, display a message
-            if (!notifications) {
-                this.setState({notifications: ["No notifications!"]})
+            if (results.data[0] === undefined) {
+            	noteys.push('none');
+                this.setState({notifications: noteys});
             }
             // else, set the state to their notifications as a list
             else {
-                notifications = notifications.split(",");
-                this.setState({notifications: notifications});  
+                // notifications = notifications.split(",");
+                
+
+                results.data.forEach(i =>{
+
+                	noteys.push(i.user);
+                	noteys.push(i.type);
+                })
+
+                this.setState({
+
+                	notifications: noteys
+                })
+
+                console.log(this.state.notifications);
+
+
+              	
             }
         })
     }
@@ -33,27 +58,48 @@ class Notifications extends Component {
     render() {
 
         return (
+
+        	<div>
         <div className="row">
             <div className="col s12 top z-depth-2">
                 <h1 className="center">My Notifications</h1>
             </div>
+            </div>
+            <div className="container">
+            <div className="row">
+            <div className="col s12">
             <table>
                 <thead>
                 <tr>
-                    <th>Notification Message</th>
+                    <th>From</th>
+                    <th>Type</th>
                 </tr>
                 </thead>
                 <tbody>
-                {this.state.notifications.map(notification => {
-                    if (notification === "No notifications!") {
-                        return <tr><td>{notification}</td></tr>
-                    }
-                    else {
-                        return <tr><td>{notification}</td><td><a className="btn">Accept</a><a className="btn">Decline</a></td></tr>
-                    }
+
+                {this.state.notifications.map( (el, i) => {
+
+                	if (el === 'none') {
+
+                		return <tr>
+                        <td>No Notifications</td>
+                        </tr>
+                		
+                	}else
+                	{
+
+                		return <tr>
+                        <td>{el}</td>
+                        <td>{el}</td>
+                        <td><a className="btn">Accept</a><a className="btn">Decline</a></td>
+                        </tr>
+                  }
                 })}
                 </tbody>
             </table>
+        </div>
+        </div>
+        </div>
         </div>
         );
     }
