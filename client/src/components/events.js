75b7ed7c1777bd64a8beae349666bf0288afd97f
Ben Grant <Benjah88@gmail.com>
2018-01-22 10:19:45 -0800

    update

--- client/src/components/events.js
+++ client/src/components/events.js
@@ -14,11 +14,12 @@ render() {
   
         <div className="col s8 m7 l7">
         <div className="col s12 top z-depth-2 bordy2 hoverable">
-           <h4>Selected Group Name, Members</h4>
+           <h4>Selected Group Name, Members, Highest Voted Event</h4>
            <hr/>
            <div className="col s10 offset-s1">
            <div className="eventy">
            <p>Event 1 Here - Posted By Here</p>
+           <p className="votey">Votes: 0</p>
               <div className="coms">
            <p> Comment 1...</p>
            <p> Comment 2...</p>
@@ -29,6 +30,7 @@ render() {
            </div>
            <div className="eventy">   
            <p>Event 2 Here - Posted By Here</p>
+           <p className="votey">Votes: 0</p>
            <div className="coms">
            <p> Comment 1...</p>
            <p> Comment 2...</p>
@@ -39,6 +41,7 @@ render() {
            </div>
            <div className="eventy">
               <p>Event 3 Here - Posted By Here</p>
+              <p className="votey">Votes: 0</p>
               <div className="coms">
            <p> Comment 1...</p>
            <p> Comment 2...</p>
@@ -49,7 +52,8 @@ render() {
            </div>
 
            <div className="eventy">
-              <p>Event 4 Here - Posted By Here</p>
+              <p className="votey">Event 4 Here - Posted By Here</p>
+              <p>Votes: 0</p>
               <div className="coms">
            <p> Comment 1...</p>
            <p> Comment 2...</p>
