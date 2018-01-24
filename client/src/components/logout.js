5d6678014950a631555a5401c2055e48d7afcf81
Luis Mendoza <luismendoza@Luiss-MacBook-Pro.local>
2018-01-23 18:37:57 -0800

    added logged in and images to friends page

--- client/src/components/logout.js
+++ client/src/components/logout.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Axios from "axios";
+import axios from "axios";
 import '../App.css';
 import Cookies from 'universal-cookie';
 import Nav from './nav.js'
@@ -13,11 +13,19 @@ class Logout extends Component {
 	state = {
 		gifs: [],
 		pick: ""
-	};
+	}
+
+	update = () => {
+		var id = cookies.get('id');
+		axios.put("/logout/"+id)
+
+	}	
 
 	componentDidMount() {
+		this.update()
+
 		var key = "api_key=IelN2EsP53lCGIzF6kjIakIkgXbSa3bL"
-		Axios.get("https://api.giphy.com/v1/gifs/search?q=crying&limit=20&offset=0&rating=G&lang=en&"+key)
+		axios.get("https://api.giphy.com/v1/gifs/search?q=crying&limit=20&offset=0&rating=G&lang=en&"+key)
 			.then((data) => {
 
 				cookies.remove('name');
@@ -41,24 +49,22 @@ class Logout extends Component {
 	render() {
 
 		return (
-			<div>
-			<Nav/>
-			<div className = "wrapper">
-				
-				<div className="row">
-					<div className="logoutPanel col s12 top z-depth-2">
-						<h2 className="logoutText center">Why do you have to leave? Everyone always leaves...</h2>
-					</div>
-				</div>
-				<div className ="row">
-					<div className="col s12">
-						<div className = "panel-list center-align">
-							<img alt='img' src ={this.state.pick}/>
+				<div>
+					<Nav/>
+					<div className = "wrapper">
+						<div className="row">
+							<div className="logoutPanel col s12 top z-depth-2">
+								<h2 className="logoutText center">Why do you have to leave? Everyone always leaves...</h2>
+							</div>
+						</div>
+						<div className ="row">
+							<div className="col s12">
+								<div className = "panel-list center-align">
+									<img alt='img' src ={this.state.pick}/>
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
-				
-			</div>
 				</div>
 		);
 	};
